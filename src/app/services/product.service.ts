58b7c5682db7aff07b2e7aa0346992f5425ba63b
manan.pandya <manan.pandya@globallogic.com>
2025-02-26 18:23:11 +0530

    Upto Section 13 Search by category dynamically

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,25 +1,48 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Product } from '../common/product';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private baseUrl='http://localhost:8090/api/products';
-  constructor(private httpClient:HttpClient) { }
+  private baseUrl = 'http://localhost:8090/api/products';
 
-  getProductList():Observable<Product[]>
-  {
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(map(response=>response._embedded.products));
+  private categoryUrl = 'http://localhost:8090/api/product-category';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProductList(theCategoryId: number): Observable<Product[]> {
+
+    // need to build URL based on category id 
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+  }
+
+}
+
+interface GetResponseProducts {
+  _embedded: {
+    products: Product[];
   }
 }
-interface GetResponse
-{
-  _embedded:
-  {
-    products:Product[];
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
